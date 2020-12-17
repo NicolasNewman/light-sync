d5958aa101ff1968bdcd593d6c8b4bab28b89555
NicolasNewman <nanewman01@gmail.com>
2020-12-17 12:31:07 -0600

    added visualizer to build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 rm -rf ./build ./dist
-pyinstaller --onefile --paths ./venv/Lib/site-packages light_sync.py frame_color_lib.py convertor_lib.py
+pyinstaller --onefile --paths ./venv/Lib/site-packages light_sync.py frame_color_lib.py convertor_lib.py visualizer.py
